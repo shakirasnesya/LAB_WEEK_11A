2ff9fd2189d6fd041c20043251ad22dbb21ff704
shakiranesya <shakira.salvia@student.umn.ac.id>
2025-12-25 09:07:24 +0700

    Commit No. 1: Building App with SharedPreferences and DataStore

--- app/src/main/java/com/example/lab_week_11a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_11a/MainActivity.kt
@@ -1,48 +1,29 @@
 package com.example.lab_week_11
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModel
-import android.widget.TextView
-import android.widget.EditText
-import android.widget.Button
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val preferenceWrapper = (application as
-                SettingsApplication).settingsStore
 
-        val preferenceViewModel = ViewModelProvider(this, object :
-            ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return SettingsViewModel(preferenceWrapper) as T
+class SettingsViewModel(private val settingsStore: SettingsStore) :
+    ViewModel(){
+    private val _textLiveData = MutableLiveData<String>()
+    val textLiveData: LiveData<String> = _textLiveData
+
+    init {
+        viewModelScope.launch {
+            settingsStore.text.collect {
+                _textLiveData.value = it
             }
-        })[SettingsViewModel::class.java]
-
-        preferenceViewModel.textLiveData.observe(this) {
-            findViewById<TextView>(
-                R.id.activity_main_text_view
-            ).text = it
         }
+    }
 
-        findViewById<Button>(R.id.activity_main_button).setOnClickListener {
-            preferenceViewModel.saveText(
-                findViewById<EditText>(R.id.activity_main_edit_text)
-                    .text.toString()
-            )
+    fun saveText(text: String) {
+        viewModelScope.launch {
+            settingsStore.saveText(text)
         }
     }
 }
